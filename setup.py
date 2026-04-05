de51c9252471f4d2807f50144eed3db3216b6638
Indominous05 <p252305@gmail.com>
2026-04-06 00:46:53 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="GenAI Project",
+    version="0.1.0",
+    author="Parth Patil",
+    author_email="parthpatil.051104@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
